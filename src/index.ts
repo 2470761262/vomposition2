c6ecbef03278ca3cd95036b1526f8b62184c7ec1
247076126@qq.com <247076126@qq.com>
2020-11-09 14:51:46 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,78 @@
 import {
-  Iv2Root,
-  callback,
-  ComponentOptions,
-  WatchOptions,
-  vue,
+    Iv2Root,
+    callback,
+    ComponentOptions,
+    WatchOptions,
+    vue,
 } from "./inedx";
 class Vcomposition2 {
-  vm: Iv2Root;
-  root: any;
-  constructor(vm: Iv2Root) {
-    this.vm = vm;
-    this.root = this.vm.root;
-  }
+    vm: Iv2Root;
+    root: any;
+    constructor(vm: Iv2Root) {
+        this.vm = vm;
+        this.root = this.vm.root;
+    }
 
-  onCreate(callback: callback) {
-    this.vm.$on("hook:created", () => {
-      const methods: any = callback();
-      methods && (Object as any).assign(this.vm, methods);
-    });
-  }
+    onCreate(callback: callback) {
+        this.vm.$on("hook:created", () => {
+            const methods: any = callback();
+            methods && (Object as any).assign(this.vm, methods);
+        });
+    }
 
-  onMounted(callback: callback) {
-    this.vm.$on("hook:mounted", () => {
-      const methods: any = callback();
-      methods && (Object as any).assign(this.vm, methods);
-    });
-  }
+    onMounted(callback: callback) {
+        this.vm.$on("hook:mounted", () => {
+            const methods: any = callback();
+            methods && (Object as any).assign(this.vm, methods);
+        });
+    }
 
-  onUnMounted(callback: callback) {
-    this.vm.$on("hook:beforeDestroy", callback);
-  }
+    onUnMounted(callback: callback) {
+        this.vm.$on("hook:beforeDestroy", callback);
+    }
 
-  onDestroyed(callback: callback) {
-    this.vm.$on("hook:destroyed", callback);
-  }
+    onDestroyed(callback: callback) {
+        this.vm.$on("hook:destroyed", callback);
+    }
 
-  onWatch(exp: string, cb: callback, options: WatchOptions) {
-    return this.vm.$watch("root." + exp, cb, options);
-  }
+    onWatch(exp: string, cb: callback, options: WatchOptions) {
+        return this.vm.$watch("root." + exp, cb, options);
+    }
 
-  onComputed(key: string, callback: callback) {
-    this.vm.$watch(
-      callback,
-      function (newVal) {
-        this.$set(this.cd, key, newVal);
-      },
-      {
-        deep: true,
-        immediate: true,
-      }
-    );
-  }
+    onComputed(key: string, callback: callback) {
+        this.vm.$watch(
+            callback,
+            function (newVal) {
+                this.$set(this.cd, key, newVal);
+            },
+            {
+                deep: true,
+                immediate: true,
+            }
+        );
+    }
 
-  ref(key: string, value: any) {
-    this.vm.$set(this.vm.root, key, value);
-  }
+    ref(key: string, value: any) {
+        this.vm.$set(this.vm.root, key, value);
+    }
 }
 
 const V2Init = <T extends vue>(
-  vmOptions: ComponentOptions<T>
+    vmOptions: ComponentOptions<T>
 ): ComponentOptions<T> => {
 
-  const optionsData = vmOptions.data ? (vmOptions as any).data() : {}
+    //这里会提示没有调用签名,改如何解决
+    //没有没有大佬带带
+    const optionsData = vmOptions.data ? (vmOptions as any).data() : {}
 
-  vmOptions.data = function () {
-    return {
-      ...optionsData,
-      root: {},
-      cd: {},
+    vmOptions.data = function () {
+        return {
+            ...optionsData,
+            root: {},
+            cd: {},
+        };
     };
-  };
-  return vmOptions;
+    return vmOptions;
 };
 
 export { Vcomposition2, V2Init };
