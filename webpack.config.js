e143fcff7e176ebf487a32a880cf84218bbc2df4
247076126@qq.com <247076126@qq.com>
2020-11-09 20:43:15 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
 const path = require("path");
+const fs = require("fs")
 process.env.NODE_ENV = 'production';//production
+
+//自动增加package.join中的version
+const package = JSON.parse(fs.readFileSync("package.json").toString());
+const version = package.version;
+package.version = Array.from(new String(parseInt(version.replace(/\./g, "")) + 1)).join(".");
+fs.writeFileSync('package.json', JSON.stringify(package,null,4));
+
 module.exports = {
-    
-  //  devtool: "eval-source-map",
+
+    devtool: "eval-source-map",
 
     entry: {
         main: path.join(__dirname, "./src/index.ts"),
@@ -12,7 +20,7 @@ module.exports = {
 
     module: {
         rules: [
-            {test: /\.ts$/, loader: "ts-loader"}
+            { test: /\.ts$/, loader: "ts-loader" }
         ]
     },
     // 输出
@@ -20,7 +28,7 @@ module.exports = {
         path: path.resolve(process.cwd(), "./dist"),
         publicPath: "",
         filename: "[name].js",
-        libraryTarget:"umd"
+        libraryTarget: "umd"
     },
     // devServer: {
 
