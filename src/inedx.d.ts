4274e80fdbf79b73ed7ae449ae56dde6e4be170c
247076126@qq.com <247076126@qq.com>
2020-11-09 16:51:01 +0800

    update

--- src/inedx.d.ts
+++ src/inedx.d.ts
@@ -1,33 +1,33 @@
 import vue, { ComponentOptions, WatchOptions } from "vue";
 
 export interface Iv2Root extends vue {
-  root: any;
-  cd: any;
+    root: any;
+    cd: any;
 }
 export type callback = (...arg: any[]) => any;
 
 export class Vcomposition2 {
-  constructor(vm: Iv2Root);
+    constructor(vm: Iv2Root);
 
-  onCreate(callback: callback): void;
+    onCreate(callback: callback): void;
 
-  onMounted(callback: callback): void;
+    onMounted(callback: callback): void;
 
-  onUnMounted(callback: callback): void;
+    onUnMounted(callback: callback): void;
 
-  onDestroyed(callback: callback): void;
+    onDestroyed(callback: callback): void;
 
-  onWatch(exp: string, cb: callback, options: WatchOptions): callback;
+    onWatch(exp: string, cb: callback, options: WatchOptions): callback;
 
-  onComputed(key: string, callback: callback): void;
+    onComputed(key: string, callback: callback): void;
 
-  ref(key: string, value: any): void;
+    ref(key: string, value: any): void;
 
-  use(callback:callback):Vcomposition2;
+    use(callback: callback | Array<callback>): Vcomposition2;
 }
 
 export const V2Init: <T extends vue>(
-  vmOptions: ComponentOptions<T>
+    vmOptions: ComponentOptions<T>
 ) => ComponentOptions<T>;
 
 export { ComponentOptions, WatchOptions, vue };
